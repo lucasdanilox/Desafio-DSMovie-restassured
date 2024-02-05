0ccb7fa6b1c7e5a0dadf4cfd0780abced23c145b
lucasdanilox <lukastecno@gmail.com>
2024-02-04 21:46:05 -0300

    Tests findAll

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -3,17 +3,44 @@ package com.devsuperior.dsmovie.controllers;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class MovieControllerRA {
+
+    void setUp() {
+        baseURI = "http://localhost:8080";
+    }
 
     @Test
     public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
 
+        given()
+                .get("/movies")
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(1))
+                .body("content.title[0]", equalTo("The Witcher"))
+                .body("content.score[0]", is(4.33F))
+                .body("content.count[0]", is(3))
+                .body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
     }
 
     @Test
     public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
 
+        given()
+                .get("/movies?title=titanic")
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(7))
+                .body("content.title[0]", equalTo("Titanic"))
+                .body("content.score[0]", is(0.0F))
+                .body("content.count[0]", is(0))
+                .body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yDI6D5ZQh67YU4r2ms8qcSbAviZ.jpg"));
+        
     }
 
     @Test
