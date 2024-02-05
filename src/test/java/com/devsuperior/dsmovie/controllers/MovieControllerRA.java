07c47abd9169ba7c13acc1e1ce7c323762040a24
lucasdanilox <lukastecno@gmail.com>
2024-02-04 21:53:02 -0300

    Tests findById

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
@@ -10,8 +11,15 @@ import static org.hamcrest.Matchers.is;
 
 public class MovieControllerRA {
 
+    private Long movieExistingId, nonMovieExistingId;
+
+    @BeforeEach
     void setUp() {
         baseURI = "http://localhost:8080";
+
+        movieExistingId = 1L;
+        nonMovieExistingId = 100L;
+
     }
 
     @Test
@@ -40,15 +48,32 @@ public class MovieControllerRA {
                 .body("content.score[0]", is(0.0F))
                 .body("content.count[0]", is(0))
                 .body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yDI6D5ZQh67YU4r2ms8qcSbAviZ.jpg"));
-        
+
     }
 
     @Test
     public void findByIdShouldReturnMovieWhenIdExists() {
+
+        given()
+                .get("/movies/{id}", movieExistingId)
+                .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("title", equalTo("The Witcher"))
+                .body("score", is(4.33F))
+                .body("count", is(3))
+                .body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
+
+
     }
 
     @Test
     public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+
+        given()
+                .get("/movies/{id}", nonMovieExistingId)
+                .then()
+                .statusCode(404);
     }
 
     @Test
